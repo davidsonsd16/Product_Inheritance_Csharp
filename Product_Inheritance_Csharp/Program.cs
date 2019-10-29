9360b7e44a9ab3d46f5000412785588974d8c36f
Davidson Dias <davidsonsd16@gmail.com>
2019-10-28 21:50:08 -0300

    Class Product

---
+++ Product_Inheritance_Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Product_Inheritance_Csharp.Entities;
+
+namespace Product_Inheritance_Csharp {
+    class Program {
+        static void Main(string[] args) {
+
+            Product product = new Product("TV", 1200.00);
+            Console.WriteLine(product.PriceTag());
+        }
+    }
+}
