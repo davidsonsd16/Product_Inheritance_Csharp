80c1b055d588823334237f2d07de660d880ad130
Davidson Dias <davidsonsd16@gmail.com>
2019-10-28 22:48:52 -0300

    Interface

--- Product_Inheritance_Csharp/Program.cs
+++ Product_Inheritance_Csharp/Program.cs
@@ -1,18 +1,45 @@
 ﻿using System;
 using Product_Inheritance_Csharp.Entities;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Product_Inheritance_Csharp {
     class Program {
         static void Main(string[] args) {
 
-            Product product = new Product("TV", 1200.00);
-            Console.WriteLine(product.PriceTag());
+            List<Product> list = new List<Product>();
 
-            Product product1 = new ImportedProduct("Notebook", 260.00, 20.00);
-            Console.WriteLine(product1.PriceTag());
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
 
-            Product product2 = new UsedProduct("Iphone", 400.00, DateTime.Parse("15/03/2017"));
-            Console.WriteLine(product2.PriceTag());
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'c') {
+                    list.Add(new Product(name, price));
+                }
+                else if (ch == 'u') {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product product in list) {
+                Console.WriteLine(product.PriceTag());
+            }
         }
     }
 }
